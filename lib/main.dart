a064e5c35b9c37973fe28299ebfffbebeb3b8528
Flutter-Code-Craft <muneefullah24mdswe363@gmail.com>
2026-08-18 05:06:40 -0700

    add all files

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,26 @@
-import 'package:app/splash_scree.dart';
+import 'package:app/ui/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() async {
+void main() async{
+
   WidgetsFlutterBinding.ensureInitialized();
-
   await Firebase.initializeApp();
-
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-     home: SplashScree(),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple
+      ),
+      home: SplashScreen(),
     );
   }
 }
+
